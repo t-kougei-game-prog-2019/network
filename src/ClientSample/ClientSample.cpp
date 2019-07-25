c69bd622d3b179f14016477839239542f1e1fb0f
imagire <imagire@gmail.com>
2019-07-25 10:25:08 +0900

    課題の追加

---
+++ src/ClientSample/ClientSample.cpp
@@ -0,0 +1,60 @@
+﻿// ClientSample.cpp
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+int main(void) 
+{
+	const WCHAR destination[] = L"127.0.0.1";	//接続するサーバのIPアドレス
+	const int PORT_NO = 7000;	//ポート番号はサーバプログラムと共通
+
+	//ソケット通信の準備
+	WSADATA data;
+	WSAStartup(MAKEWORD(2, 0), &data);
+
+	// サーバのアドレス情報を設定
+	struct sockaddr_in dest;
+	memset(&dest, 0, sizeof(dest));
+	****
+	InetPton(****);
+
+	// ソケットの生成
+	SOCKET s = socket(AF_INET, SOCK_STREAM, 0);
+
+	//サーバへの接続
+	if (****) {
+		printf("%lsに接続できませんでした\n", destination);
+		return -1;
+	}
+	printf("接続しました: %ls\n", destination);
+
+	while(1){
+		// サーバにデータを送信
+		printf("サーバに送信する文字列を入力して下さい(「bye」なら終了)\n");
+
+		char send_data[1024];
+		scanf_s("%s", send_data, 1024);
+		if (****) {
+			printf("送信エラー\n");
+		}
+
+		// "bye"で終了
+		if (strcmp("bye", send_data) == 0) break;
+
+		// サーバからデータを受信
+		char buffer[1024];
+		if (****) {
+			printf("受信エラー\n");
+		}
+		printf("応答がきました: %s\n", buffer);
+	};
+
+	// ソケット通信の終了
+	printf("接続終了\n");
+	****
+
+	return 0;
+}
---
+++ src/NetworkSample/ServerSample.cpp
@@ -0,0 +1,82 @@
+﻿// ServerSample.cpp
+
+#include "pch.h"
+#include <iostream>
+#include <winsock2.h>
+
+int main()
+{
+	// 接続を許可するクライアント端末の情報
+	const int PORT_NO = 7000;	//ポート番号はクライアントプログラムと共通
+
+	SOCKET s, s1;         //ソケット
+
+	struct sockaddr_in source;
+	char ans[] = "送信成功";
+
+	// 待機情報を登録
+	memset(&source, 0, sizeof(source));
+	source.sin_family = AF_INET;
+	source.sin_port = htons(PORT_NO);
+	source.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// ソケット通信の準備
+	WSADATA data;
+	if (WSAStartup(MAKEWORD(2, 0), &data) < 0) {
+		printf("%d\n", GetLastError());
+		printf("ソケット通信準備エラー\n");
+	}
+
+	// ソケットの生成
+	s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s < 0) {
+		printf("%d\n", GetLastError());
+		printf("ソケット生成エラー\n");
+	}
+
+	//ソケットのバインド
+	if (bind(s, (struct sockaddr *)&source, sizeof(source)) < 0) {
+		printf("%d\n", GetLastError());
+		printf("バインドエラー\n");
+	}
+
+	//接続の許可
+	if (listen(s, 1) < 0) {
+		printf("接続許可エラー\n");
+
+	}
+
+	printf("接続開始\n");
+
+	//クライアントから通信があるまで待機
+	s1 = accept(s, NULL, NULL);
+	if (s1 < 0) {
+		printf("待機エラー\n");
+	}
+
+	while (1) {
+		//クライアントから送信されたデータの受信
+		char buffer[1024];
+		if (recv(s1, buffer, sizeof(buffer), 0) < 0) {
+			printf("受信エラー\n");
+		}
+		printf("%sを受信しました\n", buffer);
+
+		// "bye"なら終了
+		if (strcmp("bye", buffer) == 0) break;
+
+		//クライアントへデータを送信する
+		char send_data[1024+18];
+		sprintf_s(send_data, "受信されました: %s\n", buffer);
+		if (send(s1, send_data, strlen(send_data) + 1, 0) < 0) {
+			printf("送信エラー\n");
+		}
+	}
+
+	// ソケット通信の終了
+	printf("接続終了\n");
+	closesocket(s1);
+	WSACleanup();
+
+	return 0;
+}
